2bf9ddf3fca3b724622a64d52699ca8198b58b04
ASHISH SINGH <156112845+iamashishshakya@users.noreply.github.com>
2026-01-20 11:36:18 +0530

    Delete app directory

--- app/src/main/java/com/newproject/app/dto/ReqLogDto.java
+++
@@ -1,9 +0,0 @@
-package com.newproject.app.dto;
-
-import lombok.Data;
-
-@Data
-public class ReqLogDto {
-    private String email;
-    private String password;
-}
--- app/src/main/java/com/newproject/app/dto/ResLogDto.java
+++
@@ -1,9 +0,0 @@
-package com.newproject.app.dto;
-
-import lombok.Data;
-
-@Data
-public class ResLogDto {
-    private String token;
-    private String message;
-}
